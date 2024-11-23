8a477a1abfed15315ee005deaad65aaf40ad902c
tejnadasari <dasaritejna@gmail.com>
2024-11-22 18:43:49 -0600

    back to 92%

--- transformer_utils.py
+++ transformer_utils.py
@@ -15,21 +15,14 @@ class SelfAttention(nn.Module):
         self.softmax = nn.Softmax(dim=2)
 
     def forward(self, x):
-        # 1. Compute query, key and value
-        Q = self.W_query(x)  # (batch, num_tokens, query_dim)
-        K = self.W_key(x)  # (batch, num_tokens, key_dim)
-        V = self.W_value(x)  # (batch, num_tokens, value_dim)
+        Q = self.W_query(x)
+        K = self.W_key(x)
+        V = self.W_value(x)
 
-        # 2. Compute scaled dot product
-        # (batch, num_tokens, num_tokens) = (batch, num_tokens, key_dim) @ (batch, key_dim, num_tokens)
         scale = torch.sqrt(torch.tensor(self.query_dim, dtype=torch.float32))
+
         attention_scores = torch.bmm(Q, K.transpose(1, 2)) / scale
-
-        # 3. Apply softmax
         attention_probs = self.softmax(attention_scores)
-
-        # 4. Multiply with value
-        # (batch, num_tokens, value_dim) = (batch, num_tokens, num_tokens) @ (batch, num_tokens, value_dim)
         attn_output = torch.bmm(attention_probs, V)
 
         return attn_output
@@ -43,15 +36,10 @@ class LayerNorm(nn.Module):
         self.input_dim = input_dim
         self.eps = eps
 
-        # Initialize scale and shift parameters
         self.w = nn.Parameter(torch.ones(input_dim))
         self.b = nn.Parameter(torch.zeros(input_dim))
 
     def forward(self, x: torch.Tensor):
-        """
-        x: Input of shape (batch_size, seq_len, input_dim)
-        Returns: Normalized tensor of shape (batch_size, seq_len, input_dim)
-        """
         assert x.shape[-1] == self.input_dim
 
         mean = x.mean(dim=-1, keepdim=True)
